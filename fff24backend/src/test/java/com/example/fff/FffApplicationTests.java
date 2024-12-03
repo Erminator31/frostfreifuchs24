67d497e89b646b29e9d6def5ed90e759f15b9080
Ermin <ermin909090@live.com>
2024-12-03 19:13:59 +0100

    init with spring boot

---
+++ fff24backend/src/test/java/com/example/fff/FffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
