7a0a91599be28b16aaa5ef75598553f773ef121b
Ermin <ermin909090@live.com>
2024-12-10 18:31:48 +0100

    database method corrected

--- fff24backend/src/main/java/com/example/fff/databse/PostgresDBProductManagement.java
+++ fff24backend/src/main/java/com/example/fff/databse/PostgresDBProductManagement.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
 public class PostgresDBProductManagement implements ProductManager {
     // Database connection details
 // Database connection details
-    String databaseURL = "jdbc:postgresql://c7ul1tn6bwsodf.cluster-czz5sOkz4scl.eu-west-1.rds.amazonaws.com:5432/d1t207hd56v54?sslmode=require";
+    String databaseURL = "jdbc:postgresql://c7u1tn6bvsodf.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d1t207hd56v54?sslmode=require";
     String username = "u3t73itv4ifknl";
     String password = "pc8d79bc3deea2ca2b99f04d14057aeb257bac91186laf2c1c3f890ffecaa803c";
 
