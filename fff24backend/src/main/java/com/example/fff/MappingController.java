53177aeb932532d3caba5c607e3b0f6e492abbd6
Ermin <ermin909090@live.com>
2025-01-21 20:35:21 +0100

    Update forecast calculation for improved precision
    Refined the base formula in the forecast calculation to enhance accuracy by incorporating additional factors. Also ensured consistent logging for better debugging and development insights.

--- fff24backend/src/main/java/com/example/fff/MappingController.java
+++ fff24backend/src/main/java/com/example/fff/MappingController.java
@@ -734,7 +734,7 @@
                             double seasonFactor  = getSeasonFactor(product.getProductId(), dateString);
 
                             // 2c) Grund-Forecast
-                            double base =(alpha * seasonFactor * historicalAvg) + ((beta * weatherFactor) + (gamma * (1 + seasonFactor))) / 2;
+                            double base =(alpha * historicalAvg) + ((beta * weatherFactor) + (gamma * historicalAvg *seasonFactor))) / 2;
                             LOGGER.log(Level.INFO, "Historical avg last 7 days" + historicalAvg + "Product: " + product.getProductName());
                             LOGGER.log(Level.INFO, "Weather factor: " + weatherFactor + "Season Factor:  " + seasonFactor);
                             // 2d) Falls dieses Produkt in den Top-2 und Regen > 50%, +5%
